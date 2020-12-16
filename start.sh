cb5df8e08697f17b9d60561faea4ae6b0bc75219
Muzammil Irshad <muzammil.irshad001@gmail.com>
2020-12-16 18:19:03 +0500

    Release 1.2

--- start.sh
+++ start.sh
@@ -2,10 +2,6 @@
 
 mkdir /home/models
 
-mkdir /home/logs
+http-server /opt/storm_gui/front_end 2>&1 &
 
-/opt/storm_gui/back_end $LICENSE_KEY > /home/logs/back_end.logs 2>&1 &
-
-http-server /opt/storm_gui/front_end > /home/logs/front_end.logs 2>&1 &
-
-tail -f /home/logs/front_end.logs -f /home/logs/back_end.logs
+/opt/storm_gui/back_end $LICENSE_KEY
